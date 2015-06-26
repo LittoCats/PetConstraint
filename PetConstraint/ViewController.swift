8f3ef858fc5e30035ab9d5e08e2ab0df28912b79
程巍巍 <littocats@gmail.com>
2015-06-26 23:28:07 +0800

    Initial Commit

---
+++ PetConstraint/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PetConstraint
+//
+//  Created by 程巍巍 on 6/26/15.
+//  Copyright © 2015 Littocats. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
